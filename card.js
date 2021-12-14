6406d3e5fbb6452e318a61c447660ba94cd1cb8b
GracieSmith359 <89494013+GracieSmith359@users.noreply.github.com>
2021-12-14 16:00:03 -0600

    Update card.js

--- card.js
+++ card.js
@@ -1,12 +1,11 @@
 "use strict";
 
-$(document).on('click', '.flip', function () {
-  let card = $(this).closest('.card');
-  if (card.hasClass('flip-it')) card.removeClass('flip-it');else card.addClass('flip-it');
+document.querySelector(document).addEventListener('click', '.flip', function () {
+  let card = document.querySelector(this).closest('.card');
+  if (card.classList.contains('flip-it')) card.removeClass('flip-it');else card.classList.add('flip-it');
 });
-$('.card').each(function () {
-  let href = $(this).data('href');
-  $(this).find('.image').css({
+document.querySelector('.card').each(function () {
+  let href = document.querySelector(this).data('href');
+  document.querySelector(this).querySelector('.image').css({
     backgroundImage: ['url(', href, ')'].join('')
   });
-});
