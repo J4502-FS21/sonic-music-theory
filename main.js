dacb178e55b21108cfada2ef570e13c883039332
GracieSmith359 <89494013+GracieSmith359@users.noreply.github.com>
2021-12-14 16:21:50 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
-"use strict";
-
-(function($) {
-  document.querySelector(function() {
-    document.querySelector('nav ul li a:not(:only-child)').click(function(e) {
-      document.querySelector(this).siblings('.nav-dropdown').toggle();
-      document.querySelector('.dropdown').not(document.querySelector(this).siblings()).hide();
+function($) {
+  $(function() {
+    $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      $('.dropdown').not($(this).siblings()).hide();
       e.stopPropagation();
     });
-    document.querySelector('html').click(function() {
-      document.querySelector('.nav-dropdown').hide();
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
     });
-    document.querySelector('#nav-toggle').click(function() {
-      document.querySelector('nav ul').slideToggle();
+    $('#nav-toggle').click(function() {
+      $('nav ul').slideToggle();
     });
-    document.querySelector('#nav-toggle').addEventListener('click', function() {
+    $('#nav-toggle').on('click', function() {
       this.classList.toggle('active');
     });
   });
-})
+})(jQuery);
